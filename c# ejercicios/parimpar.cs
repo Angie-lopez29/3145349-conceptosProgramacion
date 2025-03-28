595d57db1f4878c20b8bec0534a501011e07457f
Angie-lopez29 <xiomaragarcialopez29@gmail.com>
2025-03-28 10:27:00 -0500

    ejercicio par e impar

--- c# ejercicios/parimpar.cs
+++ c# ejercicios/parimpar.cs
@@ -1,6 +1,15 @@
 using system;
 class Program{
     static void main(){
+        int n;
+        Console.WriteLine("Ingrese un número entero:");
+        n = Convert.ToInt32(Console.ReadLine());
+        if (n % 2 == 0){
+            Console.WriteLine("El número es par.");
+        } else {
+            Console.WriteLine("El número es impar.");
+        }
+        Console.WriteLine("Fin del programa.");
         
     }
 }
